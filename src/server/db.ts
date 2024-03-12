497795cc7bf1d57042be5a44bbca1b95616522d0
Arjun Khanna <arjunkhanna0108@gmail.com>
2024-03-13 00:42:18 +0530

    chore: integrated supabase as db

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,16 +1,14 @@
-import { Client } from "@planetscale/database";
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "~/env";
 
-const psClient = new Client({ url: env.DATABASE_URL });
+// const psClient = new Client({ url: env.DATABASE_URL });
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(psClient),
+    // adapter: new PrismaPlanetScale(psClient),
   });
 
 const globalForPrisma = globalThis as unknown as {
