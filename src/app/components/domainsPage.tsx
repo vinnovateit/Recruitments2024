e93335f58df80d4edddfbbcf9a6036214ff64554
abhishekkjainn <jainabhishek1904@gmail.com>
2024-03-13 23:19:06 +0530

    Made this responsive Check how it looks in Phone before merging

--- src/app/components/domainsPage.tsx
+++ src/app/components/domainsPage.tsx
@@ -1,28 +1,27 @@
 function  DomainsPage(){
     return <>
     <div className="domainsbody h-52 w-full bg-[#1E003E] flex justify-start items-start flex-col scrollbar-none">
-    <img src="/staricon.png" alt="" />
+    <img src="/staricon.png" alt="" className="w-20 sm:scale-100" />
     </div>
     <div className="h-screen w-full bg-[#1E003E] flex justify-start items-start flex-col">
-      <div className="heading px-20 py-8 flex justify-between items-center w-full">
+      <div className="heading h-64 px-8 sm:scale-100 sm:px-20 py-4 flex justify-between flex-col sm:flex-row items-start w-full">
         <div className="headingsection">
-        <img src="/OUR.png" alt="asdf" className=""/>
-        <span className="text-[100px] font-bold text-white">DOMAINS</span>
+        <img src="/OUR.png" alt="asdf" className="w-36 h-18 sm:w-48"/>
+        <span className="text-[65px] sm:text-[100px] font-bold text-white">DOMAINS</span>
         </div>
         <div className="thundersection ">
-          <img src="/thundericon.png" alt="Thunderromaing Icons" className="scale-75 translate-y-12" />
+          <img src="/thundericon.png" alt="Thunderromaing Icons" className="scale-[0.2] sm:scale-75 -translate-y-20 translate-x-36 sm:translate-y-12 sm:translate-x-0" />
         </div>
       </div>
-      <div className="domains w-full flex justify-between items-center flex-row">
-        <div className="management px-16">
-          <img src="/managementcard.png" alt="Management" className=""  />
-      
+      <div className="domains h-auto w-full sm:w-full flex justify-center items-center flex-col sm:flex-row">
+        <div className="management px-0 sm:px-16 pb-8">
+          <img src="/managementcard.png" alt="Management" className="w-44 sm:w-80"  />
         </div>
-        <div className="technical">
-          <img src="/techcard.png" alt="Management" />
+        <div className="technical px-0 sm:px-16 pb-8">
+          <img src="/techcard.png" alt="Management" className="w-44 sm:w-80"  />
         </div>
-        <div className="design px-16">
-          <img src="/designcard.png" alt="Management" />
+        <div className="design px-0 sm:px-16">
+          <img src="/designcard.png" alt="Management" className="w-44 sm:w-80"  />
         </div>
       </div>
     </div>
