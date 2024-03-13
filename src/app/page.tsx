95b8fec4d91e4f02ff885e26bc1d35152281f844
eshita241 <eshitachokhani2003@gmail.com>
2024-03-13 19:58:38 +0530

    created the JoinOurCommunity section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import JoinOurCommunity from "./JoinOurCommunity";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b text-white">
+      <div className="container flex flex-col items-center justify-center ">
+      <JoinOurCommunity/>
+        {/*<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
         </h1>
+        
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
@@ -30,7 +33,7 @@ export default function HomePage() {
               deploy it.
             </div>
           </Link>
-        </div>
+  </div>*/}
       </div>
     </main>
   );
