4ae931ef99622882ba5aebfb50eb180154468304
Arjun Khanna <arjunkhanna0108@gmail.com>
2024-03-19 21:36:12 +0530

    fix: build errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import { Suspense } from "react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,8 +20,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
-    </html>
+    <Suspense>
+      <html lang="en">
+        <body className={`font-sans ${inter.variable}`}>{children}</body>
+      </html>
+    </Suspense>
   );
 }
