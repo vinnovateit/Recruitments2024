bbbcc3d3a67087b271bb14c70ead44d4d1b2742b
Ayush Kumar <theofficialayush.kumar@gmail.com>
2025-02-19 10:42:04 +0530

    minor styling fixes

--- src/components/DomainSelectionForm.tsx
+++ src/components/DomainSelectionForm.tsx
@@ -84,37 +84,7 @@ const DomainSelectionForm: React.FC = () => {
 
   return (
     <>
-    <div className="absolute top-0 left-0">
-        <svg
-          className="w-16 md:w-32 lg:w-40 ml-[-1.2vw]"
-          viewBox="0 0 129 95"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g filter="url(#filter0_d_56_3802)">
-            <path
-              d="M3.94072 3.27278C3.94072 3.27278 -7.6574 33.1742 7.87169 43.32C23.4008 53.4659 27.0135 18.6703 42.7494 27.7032C56.6596 35.688 36.1137 52.3997 54.2856 62.1731C72.4575 71.9464 83.7558 50.5554 83.7558 50.5554C83.7558 50.5554 88.4395 38.1416 74.4525 39.7015C60.4654 41.2614 57.6788 54.8699 78.6939 66.248C99.709 77.6261 108.971 62.9123 108.971 62.9123"
-              stroke="white"
-              strokeWidth="30"
-              strokeLinecap="round"
-            />
-            <path
-              d="M3.94072 3.27278C3.94072 3.27278 -7.6574 33.1742 7.87169 43.32C23.4008 53.4659 27.0135 18.6703 42.7494 27.7032C56.6596 35.688 36.1137 52.3997 54.2856 62.1731C72.4575 71.9464 83.7558 50.5554 83.7558 50.5554C83.7558 50.5554 88.4395 38.1416 74.4525 39.7015C60.4654 41.2614 57.6788 54.8699 78.6939 66.248C99.709 77.6261 108.971 62.9123 108.971 62.9123"
-              stroke="#F94AC9"
-              strokeWidth="15"
-              strokeLinecap="round"
-            />
-          </g>
-        </svg>
-      </div>
-
-      <div className="absolute top-0 right-0">
-        <img 
-          src="/thundericon.png" 
-          alt="Thunder Icon" 
-          className="w-20 md:w-32"
-        />
-      </div>
+    
     <div className="font-Fixture flex justify-center items-center min-h-screen">
       <div className="w-full max-w-4xl relative px-6">
         <div className='mt-[20vh]'>
